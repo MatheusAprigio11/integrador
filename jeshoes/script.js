00fa5c8eed619f948d5a98a8fff5e186d9c98783
Matheus Aprigio <126816425+MatheusAprigio11@users.noreply.github.com>
2023-05-29 11:30:42 -0300

    Add files via upload

---
+++ jeshoes/script.js
@@ -0,0 +1,61 @@
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+cartIcon.onclick = () =>{
+    cart.classList.add('active');
+};
+closeCart.onclick = () =>{
+    cart.classList.remove('active');
+};
+
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready);
+}else{
+    ready();
+}
+
+function ready(){
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons)
+    for (var i = 0; i < removeCartButtons.length; i++){
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+    var quantityInputs = document.getElementsByClassName('cart-quantity')
+    for (var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged);
+    } 
+}   
+
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updateTotal();
+}
+
+function quantityChanged(event){
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+    }
+    updateTotal();
+}
+
+function updateTotal(){
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++){
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+        var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+        var price = parseFloat(priceElement.innetText.replace("$", ""))
+        var quantity = quantityElement.value;
+        total = total + price * quantity;
+
+        document.getElementsByClassName("total-price")[0].innetText = '$' + total;
+
+    }
+}
